17fcd70c5c5d75d0621ef3566ffcdf53b0292a88
Carlos Sandoval <carlitoxsandoval8706@gmail.com>
2026-04-07 16:01:40 -0600

    Setting up

--- bowl.py
+++ bowl.py
@@ -1,4 +1,16 @@
 import sys
 
-with open(sys.argv[1], 'r') as file:
-    line = file.readline().split()
+def minSol(inputM):
+    horseX, horseY = 0, 0
+    for i in range(len(inputM)):
+        for j in range(len(inputM[i])):
+            if inputM[i][j] == 'H':
+                horseX = i
+                horseY = j
+
+
+def readInput():
+    print("fix me")
+
+if __name__ == "__main__":
+    print("hello")
