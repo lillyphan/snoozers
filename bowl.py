e90ef36e7d09fb110b403d724d5ddbd206f8381a
Carlos Sandoval <carlitoxsandoval8706@gmail.com>
2026-04-07 16:50:44 -0600

    Reading input works

--- bowl.py
+++ bowl.py
@@ -1,5 +1,3 @@
-import sys
-
 def minSol(inputM):
     horseX, horseY = 0, 0
     for i in range(len(inputM)):
@@ -9,8 +7,35 @@ def minSol(inputM):
                 horseY = j
 
 
-def readInput():
-    print("fix me")
+def readFromFile(inFile):
+    i = 0
+    walls = 0
+    inputM  = []
+    portals = []
+    with open(inFile, 'r') as f:
+        for line in f:
+            if i == 0:
+                walls = int(line.strip())
+            elif i == 1:
+                dims = line.strip().split()
+                rows = int(dims[0])
+                cols = int(dims[1])
+                for j in range(rows):
+                    inputM.append([])
+            elif i < rows+2:
+                inputM[i-2] = list(line.strip())
+            elif i == rows+2:
+                numPortals = int(line.strip())
+                for j in range(numPortals):
+                    portals.append([])
+            else:
+                portals[i-3-rows] = line.strip().split()
+            i += 1
+    return walls, inputM, portals
 
 if __name__ == "__main__":
-    print("hello")
+    numWalls, teamIn, portals = readFromFile("our_new_input.txt")
+    for row in teamIn:
+        print(row)
+    for portal in portals:
+        print(portal)
