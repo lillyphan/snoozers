b7596b26b04a631a5693d3cf806233294e87876f
lillyphan <lilly_phan@mines.edu>
2026-04-07 20:14:08 -0600

    add scoring function

--- bowl.py
+++ bowl.py
@@ -1,12 +1,3 @@
-def minSol(inputM):
-    horseX, horseY = 0, 0
-    for i in range(len(inputM)):
-        for j in range(len(inputM[i])):
-            if inputM[i][j] == 'H':
-                horseX = i
-                horseY = j
-
-
 def readFromFile(inFile):
     i = 0
     walls = 0
@@ -33,9 +24,66 @@ def readFromFile(inFile):
             i += 1
     return walls, inputM, portals
 
+def calcScore(inputM, portals):
+    horseX, horseY = 0, 0
+    #loop through the map to find horse position
+    for i in range(len(inputM)):
+        for j in range(len(inputM[i])):
+            if inputM[i][j] == 'H':
+                horseX, horseY = i, j
+
+    #portal map to hookup one portal to another
+    portalMap = {}
+    for portal in portals:
+        r1, c1, r2, c2 = int(portal[0]), int(portal[1]), int(portal[2]), int(portal[3])
+         #add portals as key val pairs both ways
+        portalMap[(r1, c1)] = (r2, c2)
+        portalMap[(r2, c2)] = (r1, c1)
+
+    #bfs from the horse
+    visited = []
+    queue = []
+    queue.append((horseX, horseY))
+    visited.append((horseX, horseY))
+    total = 0
+    head = 0  # points to the front of the queue
+
+    while head < len(queue):
+        x, y = queue[head]
+        head += 1
+
+        tile = inputM[x][y]
+
+        #add point values
+        if tile == '.': total += 1
+        elif tile == 'H': total += 1
+        elif tile == 'p': total += 1
+        elif tile == 'a': total += 11
+        elif tile == 'b': total += -4
+        elif tile == 'c': total += 4
+
+        #check for portal
+        if tile == 'p':
+            #get the x and y of the connecting portal
+            px, py = portalMap[(x, y)]
+            if (px, py) not in visited:
+                visited.append((px, py))
+                queue.append((px, py))
+
+        #check neighbors
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for i in range(len(directions)):
+            dx, dy = directions[i]
+            #find new position
+            nx, ny = x + dx, y + dy
+            #check bounds
+            if nx >= 0 and nx < len(inputM) and ny >= 0 and ny < len(inputM[nx]):
+                if (nx, ny) not in visited and inputM[nx][ny] != '#' and inputM[nx][ny] != 'W':
+                    visited.append((nx, ny))
+                    queue.append((nx, ny))
+
+    return total
+
 if __name__ == "__main__":
     numWalls, teamIn, portals = readFromFile("our_new_input.txt")
-    for row in teamIn:
-        print(row)
-    for portal in portals:
-        print(portal)
+    print(calcScore(teamIn, portals))
